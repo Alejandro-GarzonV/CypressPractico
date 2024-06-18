b6a2c752962db9683fd577aa1f34c7209d4f9ec3
Alejandro Garzón <117865764+Alejandro-GarzonV@users.noreply.github.com>
2024-06-18 16:26:41 -0500

    create  home page

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,19 +1,22 @@
+import { HomeMethods } from "./Pages/Home/home.methods";
 import { loginMethods } from "./Pages/Login/login.methods";
 
 Cypress.on('uncaught:exception', (err, runnable) => {
   // Retorna false para evitar que Cypress falle la prueba
   return false;
 });
-describe('Login', () => {
-  it('Login con credenciales', () => {
+describe('Demoblaze', () => {
+  it('Paginas  de demoblaze', () => {
     cy.visit('https://www.demoblaze.com/')
     cy.viewport(1280, 720)
     loginMethods.login('Pruebas900','Pruebas900')
     cy.wait(2000)
     cy.contains('a','Log out').should('be.visible').then(tittle =>{
-      cy.log(tittle.attr('class'))
-      })
+    cy.log(tittle.attr('class'))
+     })
     cy.get('#nameofuser').should('exist').should('contain.text','Pruebas900') 
+    cy.wait(2000)
+    HomeMethods.clickOnProductLink('Iphone 6 32gb')
     cy.wait(2000)
   })
 })
