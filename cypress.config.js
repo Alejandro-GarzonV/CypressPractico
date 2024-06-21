a78baca2461906987ecffae63553e366a9b65861
Alejandro Garzón <117865764+Alejandro-GarzonV@users.noreply.github.com>
2024-06-21 16:03:10 -0500

    pom y cucumber
    implementación de la  estructura correcta  pom  y   anexo de las dependencias   cucumber badeball

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
 const { configureAllureAdapterPlugins } = require('@mmisty/cypress-allure-adapter/plugins');
-import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
+//For Cucumber Integration
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin =  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 
 module.exports = defineConfig({
   //retries:1, // ESTO  REINTENTA LOS FALLIDOS PERO NO CAMBIA EL RESULTADO EN TIEMPOS
+  // configuracion del allure 
   env: {
     allure: true,
     allureResults: 'allure-results',
@@ -15,15 +19,23 @@ module.exports = defineConfig({
     allureAttachRequests: false,
     allureCompactAttachments: true,
     allureAddVideoOnPass: false,
-    tmsPrefix: 'http://jira.com',
-    issuePrefix: 'http://jira.com/PROJECT-1/*/browse'
+    //tmsPrefix: 'http://jira.com',
+    //issuePrefix: 'http://jira.com/PROJECT-1/*/browse'
   },
   e2e: {
     viewportWidth: 1280, // Ancho del viewport en píxeles
     viewportHeight: 720, // Alto del viewport en píxeles
+
+
     setupNodeEvents(on, config) {
+      // configuracion  cucumber 
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+
       on('file:preprocessor', createBundler());
       configureAllureAdapterPlugins(on, config);
+
       // Si usas el evento after:spec
       on('after:spec', async (spec, results) => {
         // tu código en after spec
@@ -32,6 +44,7 @@ module.exports = defineConfig({
       return config;
     },
     specPattern: "cypress/e2e/features/*.feature",
+    chromeWebSecurity: false,
   },
  // "pageLoadTimeout": 120000,
 });
